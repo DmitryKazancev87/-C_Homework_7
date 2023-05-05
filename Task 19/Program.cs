9acab866cc5a9eca060e1502cd0d72bda803b12f
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-05-05 23:57:17 +0300

    Черновик задания 19

---
+++ Task 19/Program.cs
@@ -0,0 +1,46 @@
+﻿// 19. Программа, которая задает двумерный массив размером m*n,
+// заполненный случайными вещественными числами.
+
+float [,] CreataArray ()
+{
+float [,] array = new float[3,4];
+for (int i=0; i< array.GetLength(0);i++)
+{
+    for (int j=0;j<array.GetLength(1);j++)
+    {
+        array[i,j]=new Random ().Next(-1,10);
+    }
+}
+return array;
+}
+void PrintArray (float [,] array)
+{
+    for (int i=0; i< array.GetLength(0);i++)
+{
+    for (int j=0;j<array.GetLength(1);j++)
+    {
+        Console.Write($"{array[i,j]} | ");
+    }
+    System.Console.WriteLine();
+}
+}
+float [,] array = CreataArray();
+PrintArray(array);
+
+
+// EnterInfomation(string message)
+//{
+//Console.Write(message);
+//return int.Parse(Console.ReadLine()!);
+//}
+//double b1 = EnterInfomation(" Ввведите значение b1 : ");
+//double k1 = EnterInfomation(" Ввведите значение k1 : ");
+//double b2 = EnterInfomation(" Ввведите значение b2 : ");
+//double k2 = EnterInfomation(" Ввведите значение k2 : ");
+// формула нахождения точки для уравнений y=k1*x+b1, y=k2*x+b2 будет выглядеть так:
+// x= (-b2+b1)/(-k1+k2) y = k2*x+b2.
+
+//double x=(-b2+b1)/(-k1+k2);
+//double y =k2*x+b2;
+
+//System.Console.Write($"Две прямые пересекуться в точке с координатами X: {x}, Y; {y}");
